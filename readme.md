f452818fcd9315611e854ed965077a0d2e20c37b
seven.l <ititonight@gmail.com>
2018-02-01 00:48:52 +0800

    - add custom base JSON pattern feature - use dot-notation to assign the response data position - use dot-notation to assign the data object extend properties - add from-list type in makePropValue

--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
-<h1>fakey-json</h1>
+<h1>fakey-json 😈</h1>
 
 # Purpose
-This is a utility for mocking json data that pretends the api response data with JSON format.
+You are a smart and proactive Frontend engineer 🤓, you complete your UI components very quickly, so that the APIs is still not ready for client. You don't have correct data to feed the UI components. Finally, You are blocked until the APIs were completed by Backend Engineers. [fakey-json] will resolve the api tasks blocking between Frontend and Backend engineers simply and easily.
+[fakey-json] is a utility for mocking json data of response in client and no need any mock server. 
+
+* **Assign custom JSON pattern for response**: You can pass any custom JSON structure to be the default value.
+* **Assign the response data on any level in JSON**: You can use dot-notation to assign the response data position so quickly.
+* **Fancy high order function pattern implementation**: [fakey-json] base on high order function pattern and return a function as a result that allow you to create new function that can be used as templates.
+* **Choose common format for the value of response property**: [fakey-json] build-in the useful and common formats for response property value.      
 
 # Installation
 ```bash
@@ -9,34 +15,87 @@ npm install fakey-json
 ```
 
 # How to use
-```ecmascript 6
+
+## For simple JSON
+Below response is your expectation:
+```js
+{
+    data: [{
+        name: "Name_1",
+        id: "19"
+    },{
+        name: "Name_2",
+        id: "20"
+    },{
+        name: "Name_3",
+        id: "21"
+    }]
+}
+```
+fakey-json can help you like:
+
+```js
 import fakeyJSON from 'fakey-json'
 
-const makeMock = fakeyJSON(undefined, {
-    summary: {
-        reseedCollected      : 9999,
-        contributionCollected: 9999,
-        totalCollected       : 9999,
-        reseedPayout         : 9999,
-        contributionPayout   : 9999,
-        totalPayout          : 9999,
-        beginningBalance     : 20000
+const base = {data: []}
+const makeMock = fakeyJSON(base, 'data')
+const responseData = makeMock(3, (index) => 
+    ({
+        name: `Name_${index}`,
+        id  : `${18 + index}`,
+    })
+)
+// responseData will be your expectation
+```
+## More complex JSON and use common format in response property value
+Below response is your expectation and you would like to make 1000 items in payload:
+```js
+{
+    data: {
+        message: "success",
+        payload: [
+            {
+                name: "Name_1",
+                createDate: '2017-01-01',
+                id: "1",
+                type: "Major"
+            },{
+                name: "Name_2",
+                createDate: '2018-01-10',
+                id: "2",
+                type: "Mini"
+            },
+            
+            ...
+            
+            {
+                name: "Name_1000",
+                createDate: '2017-08-31',
+                id: "1000",
+                type: "Major"
+            }
+        ]
+    }
+}
+```
+fakey-json can help you generate 1000 complex format easily:
+```js
+import fakeyJSON from 'fakey-json'
+
+const base = {
+    message: "success",
+    data: {
+        payload: []
+    }
+}
+const makeMock = fakeyJSON(base, 'data.payload')
+const responseData = makeMock(1000, (index, makePropValue) =>{
+    return {
+        name      : `Name_${index}`,
+        id        : `${index}`,
+        createDate: makePropValue('date', {startYear: 2017, endYear: 2018}),
+        type      : makePropValue('from-list', {list: ['Major', 'Mini', 'Minor']})
     }
 })
-
-const responseData = makeMock(10, (index, makePropValue) => {
-        return {
-            "date"                 : makePropValue('date', {startYear: 2018, endYear: 2018}),
-            "reseedCollected"      : makePropValue('currency'),
-            "contributionCollected": makePropValue('currency', {val: 100}),
-            "totalCollected"       : makePropValue('currency', {min: 100000}),
-            "reseedPayout"         : makePropValue('currency'),
-            "contributionPayout"   : makePropValue('currency'),
-            "totalPayout"          : makePropValue('currency'),
-            "reseedBalance"        : makePropValue('currency'),
-            "contributionBalance"  : makePropValue('currency'),
-            "totalBalance"         : makePropValue('currency')
-        }
-    }
-)
+// responseData will be your expectation
 ```
--- src/index.js
+++ src/index.js
@@ -1,5 +1,141 @@
 import numeral from "numeral";
 
+/*
+    const extendSetting = {
+        extendLevel: 'data',
+        extendProps: {
+            summary: {
+
+            }
+        }
+    }
+    fakeyJSON(base, 'data.payload', {extendLevel: 'data', extendProps: {}})
+*/
+
+const addExtByLevel = (base, levels, index, extendProps) => {
+    if(base[levels[index]]){
+        if(typeof base[levels[index]] === 'object'){
+            const nextIndex = index + 1
+            addExtByLevel(base[levels[index]], levels, nextIndex, extendProps)
+        }
+        if(typeof base[levels[index]] !== 'object' && index === levels.length){
+            base[levels[index]] = extendProps
+        }
+    }else{
+        Object.assign(base, extendProps)
+    }
+}
+
+const makePropValue = (type, opts) => {
+    let output = null
+
+    if(type && opts === undefined){
+        return output
+    }
+
+    const makeFinalOpts = (defaultOpts, newOpts) => {
+        let finalOpts = {}
+
+        if(newOpts && typeof newOpts !== 'object'){
+            console.warn("[fakeyJSON]: Option should be Object type")
+            finalOpts = Object.assign({}, defaultOpts)
+        }
+        if(newOpts && typeof newOpts === 'object'){
+            finalOpts = Object.assign({}, defaultOpts, opts)
+        }
+        if(newOpts === undefined){
+            finalOpts = Object.assign({}, defaultOpts)
+        }
+
+        return finalOpts
+    }
+
+    switch(type){
+        case 'from-list': {
+            let finalOpts = {}
+            const defaultOpts = {
+                list: ['']
+            }
+
+            if(opts && !opts.list){
+                console.warn("[fakeyJSON]: list property required")
+                return output
+            }
+
+            if(opts && !Array.isArray(opts.list)){
+                console.warn("[fakeyJSON]: list property should be array type")
+                return output
+            }
+            finalOpts = makeFinalOpts(defaultOpts, opts)
+            output = finalOpts.list[Math.floor(Math.random()*finalOpts.list.length)]
+            return output
+        }
+        case 'currency': {
+            let finalOpts = {}
+            const defaultOpts = {
+                min: 0,
+                max: 1000,
+                val: 1000
+            }
+
+            if(opts && opts.val){
+                opts.min = defaultOpts.min
+                opts.max = opts.val
+            }
+
+            if(opts && opts.min > opts.max){
+                console.warn("[fakeyJSON]: Min should be less than Max")
+                opts.min = defaultOpts.min
+                opts.max = defaultOpts.max
+            }
+
+            finalOpts = makeFinalOpts(defaultOpts, opts)
+            const diff = finalOpts.max - finalOpts.min
+            let currency = Math.floor(finalOpts.min) + Math.floor(Math.random()*(diff+1))
+            output = numeral(currency).format('0,0.00')
+            return output
+        }
+        case 'date': {
+            const dateObj = new Date()
+            let finalOpts = {}
+            const defaultOpts = {
+                startYear: 2000,
+                endYear: dateObj.getFullYear(),
+                isTwoDigitsMonth: true
+            }
+
+            const makeMonthAndDate = (maxNum, isTwoDigits) => {
+                let dateStr = (Math.floor(Math.random()*100) % maxNum + 1)+''
+
+                if(isTwoDigits){
+                    return (dateStr.length === 1) ? '0'+dateStr : dateStr
+                }else{
+                    return dateStr
+                }
+            }
+
+            const makeYear = (startYear, endYear) => {
+                const diff = endYear - startYear
+                return Math.floor(startYear) + Math.floor(Math.random()*(diff+1))
+            }
+
+            finalOpts = makeFinalOpts(defaultOpts, opts)
+
+            const startYear = finalOpts.startYear*1
+            const endYear = finalOpts.endYear*1
+
+            if(startYear > endYear){
+                console.warn("[fakeyJSON]: Start year should be less than End year")
+                return output
+            }
+
+            return `${makeYear(startYear, endYear)}-${makeMonthAndDate(12, finalOpts.isTwoDigitsMonth)}-${makeMonthAndDate(31, finalOpts.isTwoDigitsMonth)}`
+        }
+        default:
+            return output
+    }
+}
+
 const fakeyJSON = (base = {
     "message": "success",
     "data": {
@@ -12,104 +148,21 @@ const fakeyJSON = (base = {
         },
         "summary": null
     }
-}, dataObjExtend) => (dataNum, makeMockElement) => {
+}, payloadLevel, extendSetting) => (dataSize, makeMockElement) => {
     let dataSets = []
-    const makePropValue = (type, opts) => {
-        let output = null
-
-        const makeFinalOpts = (defaultOpts, newOpts) => {
-            let finalOpts = {}
-
-            if(newOpts && typeof newOpts !== 'object'){
-                console.warn("[fakeyJSON]: Option should be Object type")
-                finalOpts = Object.assign({}, defaultOpts)
-            }
-            if(newOpts && typeof newOpts === 'object'){
-                finalOpts = Object.assign({}, defaultOpts, opts)
-            }
-            if(newOpts === undefined){
-                finalOpts = Object.assign({}, defaultOpts)
-            }
-
-            return finalOpts
-        }
-
-        switch(type){
-            case 'currency': {
-                let finalOpts = {}
-                const defaultOpts = {
-                    min: 0,
-                    max: 1000,
-                    val: 1000
-                }
-
-                if(opts && opts.val ){
-                    opts.min = defaultOpts.min
-                    opts.max = opts.val
-                }
-
-                if(opts && opts.min > opts.max){
-                    console.warn("[fakeyJSON]: Min should be less than Max")
-                    opts.min = defaultOpts.min
-                    opts.max = defaultOpts.max
-                }
-
-                finalOpts = makeFinalOpts(defaultOpts, opts)
-                const diff = finalOpts.max - finalOpts.min
-                let currency = Math.floor(finalOpts.min) + Math.floor(Math.random()*(diff+1))
-                output = numeral(currency).format('0,0.00')
-                return output
-            }
-            case 'date': {
-                const dateObj = new Date()
-                let finalOpts = {}
-                const defaultOpts = {
-                    startYear: 2000,
-                    endYear: dateObj.getFullYear(),
-                    isTwoDigitsMonth: true
-                }
-
-                const makeMonthAndDate = (maxNum, isTwoDigits) => {
-                    let dateStr = (Math.floor(Math.random()*100) % maxNum + 1)+''
-
-                    if(isTwoDigits){
-                        return (dateStr.length === 1) ? '0'+dateStr : dateStr
-                    }else{
-                        return dateStr
-                    }
-                }
-
-                const makeYear = (startYear, endYear) => {
-                    const diff = endYear - startYear
-                    return Math.floor(startYear) + Math.floor(Math.random()*(diff+1))
-                }
-
-                finalOpts = makeFinalOpts(defaultOpts, opts)
-
-                const startYear = finalOpts.startYear*1
-                const endYear = finalOpts.endYear*1
-
-                if(startYear > endYear){
-                    console.warn("[fakeyJSON]: Start year should be less than End year")
-                    return output
-                }
-
-                return `${makeYear(startYear, endYear)}-${makeMonthAndDate(12, finalOpts.isTwoDigitsMonth)}-${makeMonthAndDate(31, finalOpts.isTwoDigitsMonth)}`
-            }
-            default:
-                return output
-        }
-    }
-
     let index = 1
-    while (index <= dataNum){
+    while (index <= dataSize){
         dataSets.push(makeMockElement(index, makePropValue))
         index ++
     }
 
-    Object.assign(base.data, dataObjExtend, {
-        payload: dataSets
-    })
+    if(extendSetting && extendSetting.extendLevel){
+        const levels = extendSetting.extendLevel.split('.')
+        addExtByLevel(base, levels, 0, extendSetting.extendProps)
+    }
+
+    const payloadLevels = payloadLevel.split('.')
+    addExtByLevel(base, payloadLevels, 0, dataSets)
 
     return base
 }
